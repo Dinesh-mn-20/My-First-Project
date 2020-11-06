b5498e43fbff013fcf07c6bf95822eddfc3d320c
JagadeeshanMS <jagadeeshanmsj@gmail.com>
2020-11-06 21:06:16 +0530

    created the function to find no of spaces

--- wordpaser.cpp
+++ wordpaser.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+
 using namespace std;
+int spacechecker(char[500],int &);
 int main()
 {
 
@@ -7,4 +9,28 @@ int main()
 
 
     
+}
+int spacechecker(char chr[500],&no_space)
+{
+    int iden1=0;
+    int iden2=0;
+    for(int i=0;i<500;i++)
+    {   
+        if(chr[i]=='/0')
+        {   
+            iden1=1;
+            no_space++;
+        }
+        if(chr[i]!='\0')
+        {
+            iden1=0;
+        }
+        if(iden1==1&&iden2==1)
+        {
+            break;
+        }
+        iden2=iden1;
+      
+    }return no_space;
+
 }
