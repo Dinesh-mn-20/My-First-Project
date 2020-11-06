261e2aa395a56e531fcbd9413ea36095c5402808
dinodinesh <73980047+dinodinesh@users.noreply.github.com>
2020-11-06 23:25:45 +0530

    Update wordpaser.cpp

--- wordpaser.cpp
+++ wordpaser.cpp
@@ -1,32 +1,27 @@
-#include<iostream>
+ #include<iostream>
 #include<string.h>
-using namespace std;
-void wordschecker(char[600],int &,int);
-int main()
-{
+#include<iomanip> 
+ using namespace std;
+ int main()
+{  int k,m=0;
    char flag[600];
-   for(int i=0;i<600;i++)
-   {
-       flag[i]=' ';
-   }
    cout<<"enter the paragraph"<<endl;
    cin.getline(flag,600);
-   int no_char=strlen(flag);
-   int no_words=1;
-   wordschecker(flag,no_words,no_char);
-   int no_letter=no_char-no_words+1;
-   printf("Analysis Report\n");
-   printf("Given paragraph contains %d words\n",no_words);
-   printf("And contains  %d letters\n",no_letter);
-   printf("On an average %.2f letters present in a word\n",no_letter/(float)no_words);
-}
-void wordschecker(char chr[600],int &no_words,int no_char)
-{  
-    for(int i=0;i<no_char;i++)
-    {
-        if(chr[i]==' ')
-        {
-            no_words++;
-        }
-    }
+   k=strlen(flag);
+    for(int i=0;i<600;i++)
+  {  
+    if(flag[i]==' ')
+      {   
+         m++;
+      }
+    if(i==k)
+      { 
+         break;
+      }
+  }
+    cout<<"the number of words in your paragraph is "<<m+1<<endl;
+    cout<<"the number of letters in your paragraph is "<<k-m<<endl;
+    float d;
+    d=(float)(k-m)/(m+1);
+    cout<<"average letters in the word is "<<setprecision(2)<<d<<endl;
 }
