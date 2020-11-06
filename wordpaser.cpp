e76f774516f4e009fd1855f022891f8e149c862a
JagadeeshanMS <jagadeeshanmsj@gmail.com>
2020-11-06 23:02:02 +0530

    works perfectly

--- wordpaser.cpp
+++ wordpaser.cpp
@@ -1,18 +1,26 @@
 #include<iostream>
-
+#include<string.h>
 using namespace std;
-void spacechecker(char[600],int &no_space);
+void wordschecker(char[600],int &no_words);
 int main()
 {
-   char flag[600]="                                                                                                                                                                                                                                                                                                            ";
+   char flag[600];
+   for(int i=0;i<600;i++)
+   {
+       flag[i]=' ';
+   }
    cout<<"enter the paragraph"<<endl;
    cin.getline(flag,600);
-   int no_space=0;
-   spacechecker(flag,no_space);
-
-   cout<<no_space<<"main";
+   int no_char=strlen(flag);
+   int no_words=0;
+   wordschecker(flag,no_words);
+   int no_letter=no_char-no_words+1;
+   printf("Analysis Report\n");
+   printf("Given paragraph contains %d words\n",no_words);
+   printf("And contains  %d letters\n",no_letter);
+   printf("On an average %.2f letters present in a word\n",no_letter/(float)no_words);
 }
-void spacechecker(char chr[600],int &no_space)
+void wordschecker(char chr[600],int &no_words)
 {  
     int iden1=0;
     int iden2=0;
@@ -23,11 +31,10 @@ void spacechecker(char chr[600],int &no_space)
         {   
             iden1=1;
             if(iden1==1&&iden2==1)
-            {   cout<<chr[i]<<"break";
+            {  
                 break;
             }
-          // cout<<chr[i]<<no_space<<"i="<<i<<"no space"<<endl;
-            no_space++;
+            no_words++;
         }
         if(chr[i]!=ch)
         {
